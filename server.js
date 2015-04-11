c786a56e85efdc705f728041741077780694f003
Stefan Gawrys <sgawrys@ic.sunysb.edu>
2015-04-11 15:37:44 -0400

    Started working on Steam OpenID authentication using passport

--- server.js
+++ server.js
@@ -1,16 +1,45 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
-
 var io = require('socket.io')(server);
+var config = require('./config');
+var passport = require('passport'),
+	SteamStrategy = require('passport-steam').Strategy;
+
 
 app.use(express.static(__dirname+"/assets/"));
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 var chat = io.of('/chat');
 
 //Associative array containing a room ID and room count of clients connected.
 var rooms = []
+//Associative array containing a room ID mapped to an array of client names.
+var users = []
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+passport.use(new SteamStrategy({
+	returnURL: 'http://localhost:8080/auth/return',
+	realm:'http://localhost:8080/',
+	apiKey: config.steamAPIKey
+},
+	function(identifier, profile, done) {
+		process.nextTick(function () {
+			console.log(identifier);
+			profile.identifier = identifier;
+			return done(null, profile);
+		});
+	}
+));
 
 var MAX_CLIENT_CONN = 2;
 var ROOM_ID_LENGTH = 5;
@@ -38,7 +67,21 @@ app.post('/room', function(req, res) {
 	rooms[newRoomID] = 0;
 
 	res.json({roomId : newRoomID});
-})
+});
+
+app.get('/auth/steam',
+	passport.authenticate('steam'),
+	function(req, res) {
+		//Doesn't matter won't get called.
+});
+
+app.get('/auth/return',
+	passport.authenticate('steam'),
+ 	function(req, res) {
+		console.log(req.isAuthenticated());
+		console.log(req.user);
+		res.render(__dirname + '/public/index.html', {user : req.user});
+});
 
 chat.on('connection', function(socket) {
 	socket.on('joinRoom', function(data) {
@@ -60,6 +103,15 @@ chat.on('connection', function(socket) {
 	socket.on('message', function(data) {
 		chat.to(socket.rooms[1]).emit('response', data);
 	});
+
+	socket.on('register', function(data) {
+		if(users[socket.rooms[1]] === undefined) {
+			users[socket.rooms[1]] = [];
+		}
+		users[socket.rooms[1]].push(data.name);
+
+		chat.to(socket.rooms[1]).emit('updateClients', { clients : users[socket.rooms[1]] });
+	})
 });
 
 server.listen(8080, function() {
